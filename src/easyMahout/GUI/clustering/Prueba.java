a87ea843c417fcb9e88e08c406fbc8d9069b766c
aldulceanu <dulceanulaurentiu@yahoo.com>
2014-01-15 15:49:52 +0100

    Empty class in order to test a clustering algorithm

--- src/easyMahout/GUI/clustering/Prueba.java
+++ src/easyMahout/GUI/clustering/Prueba.java
@@ -1,20 +1,28 @@
 package easyMahout.GUI.clustering;
 
+import org.apache.commons.math.stat.clustering.Cluster;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.*;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.clustering.classify.WeightedVectorWritable;
+import org.apache.mahout.clustering.kmeans.KMeansDriver;
+import org.apache.mahout.common.distance.EuclideanDistanceMeasure;
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.VectorWritable;
+
+
+
+import com.jidesoft.icons.IconSet.Text;
+
+import java.io.*;
+import java.util.*;
+
 public class Prueba {
-	private int a;
-
-	public int getA() {
-		return a;
-	}
-
-	public void setA(int a) {
-		this.a = a;
-	}
-
-	public Prueba(int a) {
-		super();
-		this.a = a;
-	}
 	
-
 }
