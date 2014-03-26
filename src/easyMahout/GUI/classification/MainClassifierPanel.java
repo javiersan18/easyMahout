f2af0e1a96232c202ccddd9f5383cd047079df2c
Daniel San Gabino <danielsgmoreno@gmail.com>
2014-03-26 18:05:18 +0100

    Interface changes

--- src/easyMahout/GUI/classification/MainClassifierPanel.java
+++ src/easyMahout/GUI/classification/MainClassifierPanel.java
@@ -5,62 +5,111 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JFileChooser;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
+import javax.swing.JTree;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+import easyMahout.GUI.MainGUI;
+import easyMahout.GUI.recommender.MainRecommenderPanel;
+import easyMahout.recommender.RecommenderXMLPreferences;
+import easyMahout.utils.Constants;
+import easyMahout.utils.DisabledNode;
+import easyMahout.utils.DisabledRenderer;
 import easyMahout.utils.DynamicTree;
 
 import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JSeparator;
 
 public class MainClassifierPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	
+	private final static Logger log = Logger.getLogger(MainClassifierPanel.class);
 
 	private JPanel panelClassifier;
 
 	private JPanel treePanel;
 
 	private JPanel p1, p2, p3, p4;
+	
+	private static boolean configurationModified;
 
-	private DynamicTree treeMenu;
+	private boolean controlModified;
 
-	private final static Logger log = Logger.getLogger(MainClassifierPanel.class);
+	private String activeConfigutation;
+	
+	private static String fileName;
 
-	private JSeparator separator;
+	private static JTree treeMenu;	
 
-	public MainClassifierPanel() {
-		//super();
+	private static DisabledNode nodeRoot;	
+	
+	private ArrayList<DisabledNode> treeNodes;
+
+	private DisabledNode nodeConfigure;
+
+	private DisabledNode nodeTraining;
+
+	private DisabledNode nodeAlgorithm;
+
+	private DisabledNode nodeEvaluator;
+
+	private DisabledNode nodeSaves;
+
+	private DisabledNode nodeSelected;
+
+	public MainClassifierPanel(){
 		
 		this.setLayout(null);
 		
 		panelClassifier = this;
+		
+		treeMenu = new JTree(populateTree()[0]);
+
+		DisabledRenderer renderer = new DisabledRenderer();
+		treeMenu.setCellRenderer(renderer);
+		
+		treeMenu.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent me) {
+				doMouseClicked(me);
+			}
+		});
 
 		treePanel = new JPanel();
-		treePanel.setBounds(0, 0, 220, 395);
+		treePanel.setBorder(null);
+		treePanel.setBounds(20, 11, 202, 410);
 
-		treeMenu = new DynamicTree("Classification");
-		treeMenu.setBounds(10, 11, 210, 380);
-		populateTree(treeMenu);
-		treeMenu.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		treeMenu.setBounds(0, 0, 202, 410);
+		treeMenu.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0), 1, true), "Options", TitledBorder.CENTER, TitledBorder.TOP,
+				null, null));
+		treeMenu.setRootVisible(false);
+		treeMenu.setShowsRootHandles(true);
 		treeMenu.expandRow(0);
-		setLayout(null);
+
 		treePanel.setLayout(null);
 		treePanel.add(treeMenu);
+
+		nodeRoot = (DisabledNode) treeMenu.getModel().getRoot();
+
 		this.add(treePanel);
-
-		separator = new JSeparator();
-		separator.setBounds(225, 268, 1, 2);
-		treePanel.add(separator);
-
+		
 		// Create different panes
-		// 1 panel por cada posible configuracion
+
 		p1 = new DataModelClassifierPanel();
 		p1.setBounds(238, 11, 481, 382);
 		panelClassifier.add(p1);
@@ -94,40 +143,64 @@ public class MainClassifierPanel extends JPanel {
 		this.treePanel = treePanel;
 	}
 
-	public void populateTree(DynamicTree treeMenu) {
+	public DisabledNode[] populateTree() {
 
-		String cat1 = new String("Data Model");
-		String cat2 = new String("Training Data");
-		String cat3 = new String("Algorithm");
-		String cat4 = new String("Evaluator");
-
-		treeMenu.addObject(null, cat1);
-		treeMenu.addObject(null, cat2);
-		treeMenu.addObject(null, cat3);
-		treeMenu.addObject(null, cat4);
-
-		treeMenu.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent me) {
-				doMouseClicked(me);
+		String[] categories = { "Root", // 0
+				"Configure", // 1
+				"Data Model", // 2
+				"Training Data", // 3
+				"Algorithm", // 4
+				"Evaluator", // 5
+				"Saves" //6
+		};
+		
+		treeNodes = new ArrayList<DisabledNode>();
+		for (int i = 0; i < categories.length; i++) {
+			treeNodes.add(new DisabledNode(categories[i]));
+		}
+		
+		treeNodes.get(0).add(treeNodes.get(1));
+		treeNodes.get(1).add(treeNodes.get(2));
+		treeNodes.get(1).add(treeNodes.get(3));
+		treeNodes.get(1).add(treeNodes.get(4));
+		treeNodes.get(1).add(treeNodes.get(5));
+		treeNodes.get(0).add(treeNodes.get(6));
+		
+		nodeConfigure = treeNodes.get(1);
+		nodeTraining = treeNodes.get(3);
+		nodeAlgorithm = treeNodes.get(4);
+		nodeEvaluator = treeNodes.get(5);
+		nodeSaves = treeNodes.get(6);		
+		
+		ArrayList<DisabledNode> savesNodes = getSavesFiles();
+		if (savesNodes != null) {
+			treeNodes.addAll(savesNodes);
+			for (int i = categories.length; i < treeNodes.size(); i++) {
+				nodeSaves.add(treeNodes.get(i));
 			}
-		});
-
+		}
+		
+		return treeNodes.toArray(new DisabledNode[treeNodes.size()]);
 	}
 
+///////////////////	
+//MOUSE CLICK EVENT
+///////////////////
+	
 	void doMouseClicked(MouseEvent me) {
+		//Left click
 		if (me.getButton() == MouseEvent.BUTTON1) {
-			DefaultMutableTreeNode root = (DefaultMutableTreeNode) treeMenu.getModel().getRoot();
-			try {
-				final DefaultMutableTreeNode node = (DefaultMutableTreeNode) treeMenu.getPathForLocation(me.getX(), me.getY())
-						.getLastPathComponent();
-				if (node != null) {
-					if (node.equals(root)) {
-						log.info("root");
+			try {				
+				nodeSelected = (DisabledNode) treeMenu.getPathForLocation(me.getX(), me.getY()).getLastPathComponent();
+				
+				if (nodeSelected != null) {
+					if (nodeSelected.equals(nodeConfigure)) {
+						log.info("configureNode");
+					} else if (nodeConfigure.isNodeChild(nodeSelected)) {
+						log.info("classifier configure children B1");
 
-					} else if (root.isNodeChild(node)) {
-						log.info("catsB1");
-
-						String category = (String) node.getUserObject();
+						String category = (String) nodeSelected.getUserObject();
+						
 						if (category.equals("Data Model")) {
 							log.info("dataB1");
 							p1.setVisible(true);
@@ -159,108 +232,221 @@ public class MainClassifierPanel extends JPanel {
 					}
 				}
 			} catch (Exception e1) {
-
+				// The place where we clicked is not a tree node, do nothing.
 			}
-
 		}
+		//Right click
 		if (me.getButton() == MouseEvent.BUTTON3) {
-
-			// DefaultMutableTreeNode node = (DefaultMutableTreeNode)
-			// (me.getTreePath().getLastPathComponent());
-			DefaultMutableTreeNode root = (DefaultMutableTreeNode) treeMenu.getModel().getRoot();
 			try {
-				final DefaultMutableTreeNode node = (DefaultMutableTreeNode) treeMenu.getPathForLocation(me.getX(), me.getY())
-						.getLastPathComponent();
-				if (node != null) {
-					if (node.equals(root)) {
-						log.info("root");
-
-					} else if (root.isNodeChild(node)) {
-						log.info("cats");
-
-						JPopupMenu popupMenuRoot = new JPopupMenu();
-						// addPopup(popupMenuRoot);
-						JMenuItem _new = new JMenuItem("New");
-						_new.addActionListener(new ActionListener() {
+				
+				nodeSelected = (DisabledNode) treeMenu.getPathForLocation(me.getX(), me.getY()).getLastPathComponent();
+				
+				if (nodeSelected != null) {
+					if (nodeSelected.equals(nodeSaves)) {
+						log.info("node: " + nodeSelected.toString());
+						JPopupMenu popupMenuAdd = new JPopupMenu();
+						JMenuItem addItem = new JMenuItem("Add");
+						addItem.addActionListener(new ActionListener() {
 							public void actionPerformed(ActionEvent e) {
-								String category = (String) node.getUserObject();
-								// TODO: iniciar panel para cada tipo;
-								if (category.equals("Type")) {
-									log.info("type");
-									// int i =
-									// JOptionPane.showConfirmDialog(panelRecommender,
-									// "You have a configuration open, do you really want to create a new one whitout saving changes?",
-									// "Continue whitout saving changes",
-									// JOptionPane.YES_NO_OPTION);
-									// if (i == 0) {
-									// System.exit(0);
-									// }
-									// panelRecommender.remove(configPanel);
-									// configPanel = new typeRecommenderPanel();
-									// panelRecommender.add(configPanel);
-									// configPanel.updateUI();
-									// p1.setVisible(true);
-									// p2.setVisible(false);
-									// p3.setVisible(true);
-									// p4.setVisible(true);
-									// p5.setVisible(true);
+								if (configurationModified) {
+									int dialogResult = JOptionPane.showConfirmDialog(null,
+											"The actual Classifier configuration is not saved, would you like to save it?",
+											"Save preferences",
+											JOptionPane.YES_NO_CANCEL_OPTION);
+									if (dialogResult == JOptionPane.YES_OPTION) {
+										if (StringUtils.isBlank(activeConfigutation)) {
+											// TODO poner carpeta saves por
+											// defecto en el chooser
+											JFileChooser selectedFile = new JFileChooser();
+											int i = selectedFile.showOpenDialog(MainClassifierPanel.this);
+											if (i == JFileChooser.APPROVE_OPTION) {
+												File prefs = selectedFile.getSelectedFile();
+												String absPath = prefs.getAbsolutePath();
+												RecommenderXMLPreferences.saveXMLFile(absPath);
+												MainGUI.writeResult("Preferences file saved as: " + prefs.getName(), Constants.Log.INFO);
+											} else if (i == JFileChooser.ERROR_OPTION) {
+												MainGUI.writeResult("Error saving the file", Constants.Log.ERROR);
+												log.error("Error saving preferences file");
+											}
+											// y nuevo
+											addPreferencesFile();
+											log.debug("modified, default config");
+										} else {
+											// salvar y nuevo
+											RecommenderXMLPreferences.saveXMLFile(activeConfigutation);
+											// configurationNew = true;
+											addPreferencesFile();
+											log.debug("modified, saving added configuration");
+										}
+									} else if (dialogResult == JOptionPane.NO_OPTION) {
+										// nuevo directamente
+										// configurationNew = true;
+										addPreferencesFile();
+										log.debug("modified, no save");
+									}
+								} else {
+									// añadir directamente
+									// configurationNew = true;
+									addPreferencesFile();
+									log.debug("no modified");
+								}
+								treeMenu.expandRow(8);
+							}
+						});
 
-									// } else if (category.equals("Data Model"))
-									// {
-									// log.info("data");
-									// configPanel = new typeRecommenderPanel();
-									// } else if (category.equals("Similarity"))
-									// {
-									// log.info("sim");
-									// configPanel = new typeRecommenderPanel();
-									// } else if
-									// (category.equals("Neighborhood")) {
-									// log.info("cneigats");
-									// configPanel = new typeRecommenderPanel();
-									// } else if (category.equals("Evaluator"))
-									// {
-									// log.info("eval");
-									// configPanel = new typeRecommenderPanel();
+						popupMenuAdd.add(addItem);
+						this.add(popupMenuAdd);
+						popupMenuAdd.show(me.getComponent(), me.getX(), me.getY());
+
+					} else if (nodeSaves.isNodeChild(nodeSelected)) {
+						log.info("node: " + nodeSelected.toString());
+						JPopupMenu popupMenuSaves = new JPopupMenu();
+						JMenuItem loadItem = new JMenuItem("Load");
+						loadItem.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								String filePath = ((DisabledNode) nodeSelected).getPathFile();
+								RecommenderXMLPreferences.loadXMLFile(filePath);
+								activeConfigutation = filePath;
+								configurationModified = false;
+								controlModified = true;
+								MainGUI.setSaveItemEnabled(true);
+							}
+						});
+
+						JMenuItem deleteItem = new JMenuItem("Delete");
+						deleteItem.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent e) {
+								String name = (String) nodeSelected.getUserObject();
+								String filePath = ((DisabledNode) nodeSelected).getPathFile();
+								int dialogButton = JOptionPane.YES_NO_OPTION;
+								int dialogResult = JOptionPane.showConfirmDialog(null,
+										"Would you like to delete \"" + name + "\"?",
+										"Delete File",
+										dialogButton);
+								if (dialogResult == JOptionPane.YES_OPTION) {
+									File fichero = new File(filePath);
+									if (fichero.delete()) {
+
+										treeNodes.remove(nodeSelected);
+										nodeSaves.remove(nodeSelected);
+
+										DefaultTreeModel model = new DefaultTreeModel(nodeRoot);
+
+										treeMenu.setModel(model);
+										treeMenu.expandRow(0);
+										treeMenu.expandRow(8);
+
+										if (filePath.equals(activeConfigutation)) {
+											activeConfigutation = "";
+											MainGUI.setMainTitle(activeConfigutation);
+											MainGUI.setSaveItemEnabled(false);
+											configurationModified = false;
+											controlModified = false;
+										}
+										MainGUI.writeResult("Preferences file \"" + name + "\" successfully deleted.", Constants.Log.INFO);
+
+									} else {
+										MainGUI.writeResult("Preferences file \"" + name + "\" cannot be deleted.", Constants.Log.ERROR);
+									}
 								}
 							}
 						});
 
-						JMenuItem _deleteAll = new JMenuItem("Delete All");
-						_deleteAll.addActionListener(new ActionListener() {
-							public void actionPerformed(ActionEvent e) {
-								// TODO: borrar todas las configuraciones de
-								// cierta categoria
-								String category = (String) node.getUserObject();
-								if (category.equals("Type")) {
-									log.info("type");
+						popupMenuSaves.add(loadItem);
+						popupMenuSaves.add(deleteItem);
 
-								} else if (category.equals("Data Model")) {
-									log.info("data");
-								} else if (category.equals("Similarity")) {
-									log.info("sim");
-								} else if (category.equals("Neighborhood")) {
-									log.info("cneigats");
-								} else if (category.equals("Evaluator")) {
-									log.info("eval");
-								}
-							}
-						});
-
-						popupMenuRoot.add(_new);
-						popupMenuRoot.add(_deleteAll);
-
-						this.add(popupMenuRoot);
-						popupMenuRoot.show(me.getComponent(), me.getX(), me.getY());
-
-					} else {
-						System.out.println("elems");
+						this.add(popupMenuSaves);
+						popupMenuSaves.show(me.getComponent(), me.getX(), me.getY());
 					}
 				}
 			} catch (Exception e1) {
+				// The place where we clicked is not a tree node, do nothing.
+			}
+		}
+	}
+	
+	private ArrayList<DisabledNode> getSavesFiles() {
+
+		File dir = new File(Constants.SavesPaths.CLASSIFIER);
+
+		if (dir.exists()) {
+			File[] files = dir.listFiles();
+			ArrayList<DisabledNode> nodes = new ArrayList<DisabledNode>();
+			for (int i = 0; i < files.length; i++) {
+				nodes.add(new DisabledNode(RecommenderXMLPreferences.getTagName(files[i].getPath()), files[i].getPath()));
+				System.out.println(files[i].getName());
 
 			}
+			return nodes;
 
+		} else {
+			log.error("Preferences folder (" + Constants.SavesPaths.CLASSIFIER + ") doesnt exist.");
+			return null;
 		}
 
+	}
+
+	private void addPreferencesFile() {
+		fileName = JOptionPane.showInputDialog(null, "Write a new preferences file name?", "Enter a name", JOptionPane.QUESTION_MESSAGE);
+	
+		if (fileName != null && !fileName.isEmpty()) {
+	
+			File directory = new File(Constants.SavesPaths.RECOMMENDER);
+			if (!directory.exists()) {
+				directory.mkdirs();
+			}
+
+			String filePath = Constants.SavesPaths.RECOMMENDER + fileName + Constants.SavesPaths.EXTENSION;
+	
+			activeConfigutation = filePath;
+			configurationModified = false;
+			controlModified = true;
+	
+			treeNodes.add(new DisabledNode(fileName, filePath));
+			nodeSaves.add(treeNodes.get(treeNodes.size() - 1));
+			DefaultTreeModel model = new DefaultTreeModel(nodeRoot);
+	
+			File file = new File(filePath);
+			try {
+				if (file.createNewFile())
+					System.out.println("El fichero se ha creado correctamente");
+				else
+					System.out.println("No ha podido ser creado el fichero");
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+	
+			treeMenu.setModel(model);
+			treeMenu.expandRow(0);
+			treeMenu.expandRow(9);
+	
+			MainGUI.setMainTitle(activeConfigutation);
+			MainGUI.setSaveItemEnabled(true);
+			MainGUI.writeResult("Preferences file added: " + fileName, Constants.Log.INFO);
+		}
+	}
+	
+	public boolean isConfigurationModified() {
+		return configurationModified;
+	}
+
+	public boolean isControlModified() {
+		return controlModified;
+	}
+
+	public static void setConfigurationModified(boolean configurationModified) {
+		MainClassifierPanel.configurationModified = configurationModified;
+	}
+
+	public String getActiveConfigutation() {
+		return activeConfigutation;
+	}
+
+	public void setActiveConfigutation(String activeConfigutation) {
+		this.activeConfigutation = activeConfigutation;
+	}
+
+	public static String getFileName() {
+		return fileName;
 	}
 }
